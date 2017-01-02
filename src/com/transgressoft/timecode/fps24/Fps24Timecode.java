d71216cf5a7e8b7b86580549b1957f758b8fa35c
Octavio Calleya <octaviospain@gmail.com>
2017-01-02 23:40:06 +0100

    Updated copyright

--- src/com/transgressoft/timecode/fps24/Fps24Timecode.java
+++ src/com/transgressoft/timecode/fps24/Fps24Timecode.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2016 Octavio Calleya
+/*
+ * Copyright 2016, 2017 Octavio Calleya
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
